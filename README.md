3b6d4e82395a59939c1cd185b1e6d502ab31cbab
Mohan Veeramuthu <mohan.veeramuthu@ask.com>
2020-08-22 03:21:36 -0700

    tweet model

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Twitter
+# Tweety  
+
+### Actions   
+1. Login      
+1. List Tweet      
+1. Reply Tweet    
+1. Send Tweet    
+1. Follow User    
+1. View Profile    
+1. Logout    
+
+### Domain models    
+1. Tweet   
+1. User   
+1. Role    
 
---
+++ src/main/java/com/mveeramuthu/twitter/model/Tweet.java
@@ -0,0 +1,28 @@
+package com.mveeramuthu.twitter.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.security.Timestamp;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+
+public class Tweet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @CreationTimestamp
+    private Timestamp postTime;
+    
+    //@ManyToOne
+    //private User tweetUser;
+    
+    @NotNull
+    private String content;
+}
